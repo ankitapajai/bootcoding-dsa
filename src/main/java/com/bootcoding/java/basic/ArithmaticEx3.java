cd12ecb0a1c059a70865a143272078f657ee68aa
ankitapajai <ankitapajai@gmail.com>
2023-03-31 20:30:38 +0530

    Even element of an array.....

---
+++ src/main/java/com/bootcoding/java/basic/ArithmaticEx3.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.basic;
+
+public class ArithmaticEx3 {
+        public static void main(String [] args){
+
+            int a = 12;
+            int b = 5;
+
+            System.out.println("a+b = " +(a+b));
+            System.out.println("a-b = " +(a-b));
+            System.out.println("a*b = " +(a*b));
+            System.out.println("a/b = " +(a/b));
+            System.out.println("a%b = " +(a%b));
+        }
+    }
+
