3ad516b87958b76365b92e07a4bca5e1f91a16e2
ankitapajai <ankitapajai@gmail.com>
2023-03-31 20:34:36 +0530

    Hello World program....

--- src/main/java/com/bootcoding/java/basic/HelloWorld.java
+++ src/main/java/com/bootcoding/java/basic/HelloWorld.java
@@ -2,7 +2,9 @@ package com.bootcoding.java.basic;
 
 public class HelloWorld {
         public static void main(String[]args){
+
             System.out.println("Hello World!");
+
         }
     }
 
