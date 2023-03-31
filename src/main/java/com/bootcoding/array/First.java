03f56ef12393ad6620d9ae910876e672539afc1a
ankitapajai <ankitapajai@gmail.com>
2023-03-31 19:55:57 +0530

    data types in java

---
+++ src/main/java/com/bootcoding/array/First.java
@@ -0,0 +1,36 @@
+package com.bootcoding.array;
+
+
+    public class First {
+        public static void main(String[]args){
+            // write code here
+            int num1 = 6;
+            int num2 = 5;
+            int num3 = 5;
+            int sum = num1 + num2 + num3;
+            System.out.print("The total number is : ");
+            System.out.println(sum);
+
+            int Ankita = 23;
+            int ankita = 5;
+            int sub = Ankita-ankita;
+            System.out.print("the number is: ");
+            System.out.println(sub);
+
+            byte age = 34; // literals means ek constant value which can be assign to the variable...
+            int  age2 = 56;
+            short age3 = 87;
+            long ageDino = 566666666L;
+            char ch = 'A';
+            float f1 = 5.6f; // right hand-side wali any value means literals...
+            double d1 = 4.66;
+            boolean a = true;
+            System.out.println(age);
+            String str = "Ankita";
+            System.out.println(str);
+
+
+        }
+    }
+
+
