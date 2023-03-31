8d7ec3c73a00b7992ec2b2cece9562020a258ff6
ankitapajai <ankitapajai@gmail.com>
2023-03-31 23:46:50 +0530

    PostIncrement Example in java.....

---
+++ src/main/java/com/bootcoding/java/basic/ArithmaticEx4.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.basic;
+
+public class ArithmaticEx4 {
+        public static void main(String[]args){
+
+            System.out.println("POST INCREMENT BY ONE");
+            int postIncrement = 5;
+
+            System.out.println("Initial value: " + postIncrement);
+            int result = 10 + postIncrement++;
+            System.out.println(" result (10 + postIncrement++) = " + result);
+        }
+
+    }
+
+
