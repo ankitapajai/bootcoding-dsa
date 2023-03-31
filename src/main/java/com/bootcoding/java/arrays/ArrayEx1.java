d7551d9903635c54c0e09f5d03d94b415bc0ca72
ankitapajai <ankitapajai@gmail.com>
2023-04-01 00:47:09 +0530

    PostIncrement Example in java.....

---
+++ src/main/java/com/bootcoding/java/arrays/ArrayEx1.java
@@ -0,0 +1,37 @@
+package com.bootcoding.java.arrays;
+
+
+
+    // Arrays Examples...
+    // Arrays are declare in two types....
+
+    //1) int [] age = {10,20,30,40,50};
+    //2) int [] rollno = new int [3];
+    //    rollno[0] = 13;
+    //    rollno[1] = 13;
+    //    rol
+
+    public class ArrayEx1{
+        public static void main(String [] args){
+
+            int [] age = {10,20,30,40,50};
+            int [] rollno = new int [3];
+            rollno[0] = 13;
+            rollno[1] = 15;
+            rollno[2] = 17;
+
+            System.out.println("Roll no is " +rollno[0]);
+            System.out.println("Roll no is " +rollno[1]);
+            System.out.println("Roll no is " +rollno[2]);
+            System.out.println("Age is :" + age[0]);
+        }
+    }
+
+
+
+
+
+
+
+
+
