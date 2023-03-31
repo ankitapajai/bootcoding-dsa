b5fe9abec5cad2d1feaa8778ac33ae21070ad7e4
ankitapajai <ankitapajai@gmail.com>
2023-03-31 20:25:49 +0530

    Even element of an array.....

---
+++ src/main/java/com/bootcoding/java/basic/ArithmaticEx2.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.basic;
+
+public class ArithmaticEx2{
+        public static void main(String [] args){
+            int a = 2;
+            int b = 3;
+
+            int res = (a * a) + (b * b) + 2*(a*b);
+            int res1 = (a*b)+(a/b)+(a-b);
+            System.out.println(res);
+            System.out.println(res1);
+        }
+
+    }
+
