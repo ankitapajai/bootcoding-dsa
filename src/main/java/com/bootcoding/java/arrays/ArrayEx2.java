95663dfddbdf5abcb9fc490d92dd4a8233801f11
ankitapajai <ankitapajai@gmail.com>
2023-04-01 01:15:26 +0530

    Array Example in java.......

---
+++ src/main/java/com/bootcoding/java/arrays/ArrayEx2.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java.arrays;
+
+public class ArrayEx2 {
+
+        public static void main(String [] args){
+            // using for loop.....
+
+            int[] age = {20,30,50,40,60};
+            for(int i = 0; i < age.length; i++){
+                System.out.println("Array element: " +age[i]);
+            }
+        }
+    }
+
