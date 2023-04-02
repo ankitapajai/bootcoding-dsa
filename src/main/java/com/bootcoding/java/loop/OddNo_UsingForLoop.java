2ef6c10a43338b23162e5b61aeb4b785b0ffd183
ankitapajai <ankitapajai@gmail.com>
2023-04-02 11:54:49 +0530

    Looping programs......

---
+++ src/main/java/com/bootcoding/java/loop/OddNo_UsingForLoop.java
@@ -0,0 +1,25 @@
+package com.bootcoding.java.loop;
+
+public class OddNo_UsingForLoop
+    {
+        public static void main(String [] args)
+        {
+            // WAP to print odd number from 1 to given number.....
+            // using for loop......
+            int i = 25;
+            for(i = 1; i <= 25; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    System.out.println(i);
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+
