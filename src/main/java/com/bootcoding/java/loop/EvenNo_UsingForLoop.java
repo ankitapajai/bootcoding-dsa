7fb74843ff2aa37b84cc368db04d24ad113510dc
ankitapajai <ankitapajai@gmail.com>
2023-04-02 11:49:32 +0530

    Looping programs......

---
+++ src/main/java/com/bootcoding/java/loop/EvenNo_UsingForLoop.java
@@ -0,0 +1,26 @@
+package com.bootcoding.java.loop;
+
+public class EvenNo_UsingForLoop
+    {
+        public static void main(String [] args)
+        {
+            // WAP to print even number from 1 to given number.......
+            // for loop...
+            for(int i = 0; i <= 25; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    System.out.println(i);
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+
