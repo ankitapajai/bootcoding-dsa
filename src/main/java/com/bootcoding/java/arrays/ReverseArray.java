ea6afeed3e4924547ca8f83aeb06009d21947706
ankitapajai <ankitapajai@gmail.com>
2023-04-18 08:13:23 +0530

    Reverse array.....

---
+++ src/main/java/com/bootcoding/java/arrays/ReverseArray.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        // Reverse Array without using second array.....
+        int [] arr1 = {10,20,30,40,50,60};
+        for(int i=arr1.length-1; i>=0; i--){
+            System.out.println(arr1[i]+" ");
+
+        }
+    }
+}
