d29745025f1924ad0b4c1c06952417177c9d7e38
ankitapajai <ankitapajai@gmail.com>
2023-05-02 20:45:32 +0530

    Ascii Value program.....

---
+++ src/main/java/com/bootcoding/java/LogicalProgram/AsciiValue.java
@@ -0,0 +1,19 @@
+package com.bootcoding.java.LogicalProgram;
+
+public class AsciiValue
+{
+
+        public static void main(String[] args)
+        {
+            char a = 'A';
+            char s = 'S';
+            a++;
+            s++;
+            System.out.println((int)a);
+            System.out.println((int)s);
+
+        }
+    }
+
+
+
